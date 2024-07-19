312b3f37f0ff48a971edf14aedfd1e622fc49fcd
dsantillan2018 <43114983+dsantillan2018@users.noreply.github.com>
2024-07-19 18:36:33 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,14 +1,14 @@
 variable "aws_region" {
   description = "AWS region"
-  default = "us-east-2"
+  default = "us-east-1"
 }
 
 variable "aws_type" {
   description = "AWS ec2 type"
-  default = "t3.medium"
+  default = "t2.micro"
 }
 
 variable "aws_ami" {
   description = "AWS ami"
-  default = "ami-022661f8a4a1b91cf"
+  default = "ami-0b72821e2f351e396"
 }
